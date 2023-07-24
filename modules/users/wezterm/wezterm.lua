3e74e17018451154ad84cde80792afa839ff5614
Christos Veligkos <cveligkos@gmail.com>
2023-07-24 07:30:01 +0300

    Update wezterm config

--- modules/users/wezterm/wezterm.lua
+++ modules/users/wezterm/wezterm.lua
@@ -1,22 +1,24 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Atelier Dune (base16)'
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.use_fancy_tab_bar = false
 
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 config.visual_bell = {
-  fade_in_duration_ms = 75,
-  fade_out_duration_ms = 75,
-  target = 'CursorColor',
+	fade_in_duration_ms = 75,
+	fade_out_duration_ms = 75,
+	target = "CursorColor",
 }
 
 config.disable_default_key_bindings = false
+
+config.max_fps = 165
 
 return config
