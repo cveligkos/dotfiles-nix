65942c5e94f05163b1ad47c7f1a4b063b2f76657
Christos Veligkos <cveligkos@gmail.com>
2023-08-09 07:15:20 +0300

    Fix attrset overwrite

--- lib/host.nix
+++ lib/host.nix
@@ -29,58 +29,59 @@ with builtins; {
             hardwareNixFile
           ];
 
-          config =
-            {
-              cv = systemConfig;
+          config = {
+            cv = systemConfig;
 
-              nix.settings.experimental-features = ["nix-command" "flakes"];
+            nix.settings.experimental-features = ["nix-command" "flakes"];
 
-              networking.hostName = "${name}";
+            networking.hostName = "${name}";
 
-              networking.networkmanager.enable = true;
+            networking.networkmanager.enable = true;
 
-              boot.initrd.availableKernelModules = initrdMods;
-              boot.kernelModules = kernelMods;
-              boot.kernelParams = kernelParams;
-              boot.kernelPackages = kernelPackage;
+            boot.initrd.availableKernelModules = initrdMods;
+            boot.kernelModules = kernelMods;
+            boot.kernelParams = kernelParams;
+            boot.kernelPackages = kernelPackage;
 
-              nixpkgs.pkgs = pkgs;
-              nix.settings.max-jobs = lib.mkDefault cpuCores;
+            nixpkgs.pkgs = pkgs;
+            nix.settings.max-jobs = lib.mkDefault cpuCores;
 
-              system.stateVersion = stateVersion;
+            system.stateVersion = stateVersion;
 
-              time.timeZone = "Europe/Athens";
+            time.timeZone = "Europe/Athens";
 
-              # Select internationalisation properties.
-              i18n.defaultLocale = "en_US.UTF-8";
+            # Select internationalisation properties.
+            i18n.defaultLocale = "en_US.UTF-8";
 
-              i18n.extraLocaleSettings = {
-                LC_ADDRESS = "el_GR.UTF-8";
-                LC_IDENTIFICATION = "el_GR.UTF-8";
-                LC_MEASUREMENT = "el_GR.UTF-8";
-                LC_MONETARY = "el_GR.UTF-8";
-                LC_NAME = "el_GR.UTF-8";
-                LC_NUMERIC = "el_GR.UTF-8";
-                LC_PAPER = "el_GR.UTF-8";
-                LC_TELEPHONE = "el_GR.UTF-8";
-                LC_TIME = "el_GR.UTF-8";
-              };
+            i18n.extraLocaleSettings = {
+              LC_ADDRESS = "el_GR.UTF-8";
+              LC_IDENTIFICATION = "el_GR.UTF-8";
+              LC_MEASUREMENT = "el_GR.UTF-8";
+              LC_MONETARY = "el_GR.UTF-8";
+              LC_NAME = "el_GR.UTF-8";
+              LC_NUMERIC = "el_GR.UTF-8";
+              LC_PAPER = "el_GR.UTF-8";
+              LC_TELEPHONE = "el_GR.UTF-8";
+              LC_TIME = "el_GR.UTF-8";
+            };
 
-              programs.zsh.enable = true;
+            programs.zsh.enable = true;
 
-              environment.shells = [pkgs.zsh];
-              environment.pathsToLink = ["/share/zsh"];
+            environment.shells = [pkgs.zsh];
+            environment.pathsToLink = ["/share/zsh"];
 
-              environment.systemPackages = with pkgs; [
-                neovim
-                helix
-                git
+            environment.systemPackages = with pkgs; [
+              neovim
+              helix
+              git
 
-                direnv
-                nix-direnv
-              ];
-            }
-            // passthroughConfig;
+              direnv
+              nix-direnv
+            ];
+          };
+        }
+        {
+          config = passthroughConfig;
         }
       ];
     };
