92825d7da8ab70573780556082cbb97f581747e9
Christos Veligkos <cveligkos@gmail.com>
2023-08-09 07:10:26 +0300

    Rename fonts.fonts to fonts.packages

--- modules/system/fonts/default.nix
+++ modules/system/fonts/default.nix
@@ -14,7 +14,7 @@ in {
   };
 
   config = mkIf (cfg.enable) {
-    fonts.fonts = with pkgs; [
+    fonts.packages = with pkgs; [
       (nerdfonts.override {
         fonts = [
           # "CascadiaCode"
